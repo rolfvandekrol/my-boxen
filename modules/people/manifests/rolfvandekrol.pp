64b74e005f1c9046c37a42a4a62e50f43f06bd1d
Rolf van de Krol <info@rolfvandekrol.nl>
2014-01-26 13:59:11 +0100

    personal config, git credentials

---
+++ modules/people/manifests/rolfvandekrol.pp
@@ -0,0 +1,10 @@
+class people::rolfvandekrol {
+
+  git::config::global { 'user.email':
+    value  => 'info@rolfvandekrol.nl'
+  }
+
+  git::config::global { 'user.name':
+    value  => 'Rolf van de Krol'
+  }
+}
